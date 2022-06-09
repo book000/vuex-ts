db1da90cf7bdea3740df22a8154c69c16d941475
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-06-10 01:16:49 +0900

    feat: init

---
+++ nuxt.config.js
@@ -0,0 +1,40 @@
+
+export default {
+  ssr: false,
+
+  target: "static",
+
+  head: {
+    titleTemplate: "%s - vuex-ts",
+    title: "vuex-ts",
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+
+  css: [],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: ["@nuxt/typescript-build", "@nuxtjs/vuetify"],
+
+  modules: [],
+
+  vuetify: {
+    customVariables: ["~/assets/variables.scss"],
+    theme: {
+      dark: false,
+    },
+  },
+
+  build: {},
+};
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container fill-height>
+    <v-row justify="center">
+      <v-layout justify-center>
+        <v-btn x-large @click="add()">{{ count }}</v-btn>
+      </v-layout>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data(): {
+    count: number;
+  } {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.count++;
+    },
+  },
+});
+</script>
